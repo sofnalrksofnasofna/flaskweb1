5e64d31bae2ee834dd4ddeac8f0692208ae208e5
sofnalrksofnasofna <sofnalrksofnasofna@example.com>
2019-07-08 15:58:05 +0900

    First Commit

---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [ {
+        'id': 1, 
+        'title':'Article one', 
+        'body':'lala love', 
+        'author':'Luda', 
+        'create_date':'04-09-2018', },
+        { 'id': 2, 
+        'title':'Article two', 
+        'body':'123', 
+        'author':'vasanth nagarajan', 
+        'create_date':'05-09-2018', },
+        { 'id': 3, 
+        'title':'Article three', 
+        'body':'456',
+        'author':'nagarajan vasanth', 
+        'create_date':'04-09-2018', } ]
+    return articles
